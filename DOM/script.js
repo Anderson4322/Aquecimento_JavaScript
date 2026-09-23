bba2f75e4210c7df8272bf29708a3b88f8a7ac67
Anderson4322 <anderson.ambrosio@aluno.senai.br>
2026-09-23 17:26:40 -0500

    melhora na pagina DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,4 @@
+//Usando o DOM para manipular elementos HTML
 const openModal = document.getElementById("openModal")
 const modal = document.getElementById("modal")
 const closeModal = document.getElementById("closeModal")
@@ -7,6 +8,9 @@ const btnNav2 = document.getElementById("btnNav2")
 const btnNav3 = document.getElementById("btnNav3")
 const btnNav4 = document.getElementById("btnNav4")
 const btnNav5 = document.getElementById("btnNav5")
+const formModal = document.getElementById("formModal")
+
+//Abrindo e fechando o modal
 openModal.addEventListener('click', () => {
     modal.showModal()
 })
@@ -14,10 +18,12 @@ closeModal.addEventListener('click', () => {
     modal.close()
 })
 
+//Criando Lista
 let lista = [{ nome: "teste", idade: "20" },
-    {nome:"Anderson", idade: "18"}
+{ nome: "Anderson", idade: "18" }
 ]
 
+//Percorrendo lista usando ForEach
 lista.forEach(element => {
 
     getLista.innerHTML += `
@@ -26,10 +32,37 @@ lista.forEach(element => {
     <h4>Idade:${element.idade}</h4>
     </main>
     `
+    console.log(element)
 });
 
-btnNav1.addEventListener("click", ()=>{
-    let msg = "Dom"
+//Utilizando eventos nos documentos
+let msg
+btnNav1.addEventListener("click", () => {
+    msg = "html -> body -> div -> p"
     alert(msg)
 })
+
+
+btnNav2.addEventListener("click", () => {
+    msg = "Imagem.src = 'foto.png'"
+    alert(msg)
+})
+
+
+btnNav3.addEventListener("click", () => {
+    msg = "const p = document.createElement('p'); document.body.appendChild(p)"
+    alert(msg)
+})
+
+
+btnNav4.addEventListener("click", () => {
+    msg = "botao.addEventListenner('click', minhaFuncao);"
+    alert(msg)
+})
+
+btnNav5.addEventListener("click", () => {
+    msg = "document.querySelector('h1') document.getElementById('titulo')"
+    alert(msg)
+})
+
 
